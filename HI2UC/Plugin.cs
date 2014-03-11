8b1d099b8aa035b3607fcd7465fef65325252cba
ivandrofly <ivandrofly@gmail.com>
2014-03-11 01:11:27 +0000

    HI2C Added

---
+++ HI2UC/Plugin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Nikse.SubtitleEdit.PluginLogic
+{
+    public class HI2UC : IPlugin // dll file name must "<classname>.dll" - e.g. "Haxor.dll"
+    {
+        string IPlugin.ActionType // Can be one of these: file, tool, sync, translate, spellcheck
+        {
+            get { return "tool"; }
+        }
+
+        string IPlugin.Description
+        {
+            get { return "Convert all the text inside: \"[]\", \"()\" and \"{}\" to Uppercase"; }
+        }
+
+        string IPlugin.Name
+        {
+            get { return "Hearing Impaired to Uppercase"; }
+        }
+
+        string IPlugin.Shortcut
+        {
+            get { return string.Empty; }
+        }
+
+        string IPlugin.Text
+        {
+            get { return "Hearing Impaired to Uppercase"; }
+        }
+
+        //Gets or sets the major, minor, build, and revision numbers of the assembly.
+        decimal IPlugin.Version
+        {
+            get { return 2.8M; }
+        }
+
+        string IPlugin.DoAction(Form parentForm, string subtitle, double frameRate, string listViewLineSeparatorString, string subtitleFileName, string videoFileName, string rawText)
+        {
+            subtitle = subtitle.Trim();
+            if (!string.IsNullOrEmpty(subtitle))
+            {
+                if (!string.IsNullOrEmpty(listViewLineSeparatorString))
+                    Configuration.ListViewLineSeparatorString = listViewLineSeparatorString;
+
+                var list = new List<string>();
+                foreach (string line in subtitle.Replace(Environment.NewLine, "|").Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+                    list.Add(line);
+
+                Subtitle sub = new Subtitle();
+                SubRip srt = new SubRip();
+                srt.LoadSubtitle(sub, list, subtitleFileName);
+                using (var form = new PluginForm(sub, (this as IPlugin).Name, (this as IPlugin).Description, parentForm))
+                {
+                    if (form.ShowDialog(parentForm) == DialogResult.OK)
+                        return form.FixedSubtitle;
+                }
+                return string.Empty;
+            }
+
+            MessageBox.Show("No subtitle loaded", parentForm.Text,
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return string.Empty;
+        }
+    }
+}
---
+++ HI2UC/PluginForm.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Nikse.SubtitleEdit.PluginLogic
+{
+    partial class PluginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.listViewFixes = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.checkAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uncheckAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.invertCheckToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteLineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBoxNames = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 497);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "description label";
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Cancel.Location = new System.Drawing.Point(743, 500);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(100, 23);
+            this.btn_Cancel.TabIndex = 1;
+            this.btn_Cancel.Text = "&Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConvert.Location = new System.Drawing.Point(637, 500);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(100, 23);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "&OK";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.btn_Run_Click);
+            // 
+            // listViewFixes
+            // 
+            this.listViewFixes.AllowColumnReorder = true;
+            this.listViewFixes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listViewFixes.CheckBoxes = true;
+            this.listViewFixes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader1,
+            this.columnHeader5,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listViewFixes.ContextMenuStrip = this.contextMenuStrip1;
+            this.listViewFixes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewFixes.FullRowSelect = true;
+            this.listViewFixes.GridLines = true;
+            this.listViewFixes.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewFixes.HideSelection = false;
+            this.listViewFixes.Location = new System.Drawing.Point(3, 16);
+            this.listViewFixes.Name = "listViewFixes";
+            this.listViewFixes.ShowGroups = false;
+            this.listViewFixes.Size = new System.Drawing.Size(828, 419);
+            this.listViewFixes.TabIndex = 0;
+            this.listViewFixes.UseCompatibleStateImageBehavior = false;
+            this.listViewFixes.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Apply";
+            this.columnHeader4.Width = 45;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Line #";
+            this.columnHeader1.Width = 50;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Match";
+            this.columnHeader5.Width = 81;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Actual Status";
+            this.columnHeader2.Width = 322;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "After";
+            this.columnHeader3.Width = 324;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkAllToolStripMenuItem,
+            this.uncheckAllToolStripMenuItem,
+            this.invertCheckToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.deleteLineToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(139, 114);
+            // 
+            // checkAllToolStripMenuItem
+            // 
+            this.checkAllToolStripMenuItem.Name = "checkAllToolStripMenuItem";
+            this.checkAllToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.checkAllToolStripMenuItem.Text = "Check all";
+            this.checkAllToolStripMenuItem.Click += new System.EventHandler(this.CheckTypeStyle);
+            // 
+            // uncheckAllToolStripMenuItem
+            // 
+            this.uncheckAllToolStripMenuItem.Name = "uncheckAllToolStripMenuItem";
+            this.uncheckAllToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.uncheckAllToolStripMenuItem.Text = "Uncheck all";
+            this.uncheckAllToolStripMenuItem.Click += new System.EventHandler(this.CheckTypeStyle);
+            // 
+            // invertCheckToolStripMenuItem
+            // 
+            this.invertCheckToolStripMenuItem.Name = "invertCheckToolStripMenuItem";
+            this.invertCheckToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.invertCheckToolStripMenuItem.Text = "Invert check";
+            this.invertCheckToolStripMenuItem.Click += new System.EventHandler(this.CheckTypeStyle);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.CheckTypeStyle);
+            // 
+            // deleteLineToolStripMenuItem
+            // 
+            this.deleteLineToolStripMenuItem.Name = "deleteLineToolStripMenuItem";
+            this.deleteLineToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.deleteLineToolStripMenuItem.Text = "Delete Line";
+            this.deleteLineToolStripMenuItem.Click += new System.EventHandler(this.CheckTypeStyle);
+            // 
+            // checkBoxNames
+            // 
+            this.checkBoxNames.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxNames.AutoSize = true;
+            this.checkBoxNames.Checked = true;
+            this.checkBoxNames.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxNames.Location = new System.Drawing.Point(731, 40);
+            this.checkBoxNames.Name = "checkBoxNames";
+            this.checkBoxNames.Size = new System.Drawing.Size(112, 17);
+            this.checkBoxNames.TabIndex = 3;
+            this.checkBoxNames.Text = "Names of Peoples";
+            this.toolTip1.SetToolTip(this.checkBoxNames, "This will change the Narrator Text\r\nEx: Harry: Hello! -> HARRY: Hello!");
+            this.checkBoxNames.UseVisualStyleBackColor = true;
+            this.checkBoxNames.CheckedChanged += new System.EventHandler(this.checkBoxNarrator_CheckedChanged);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Uppercase (HELLO)",
+            "Lowercase (hello)",
+            "First Char in Word (Hello Hello)",
+            "UpperLower (HeLlO)"});
+            this.comboBox1.Location = new System.Drawing.Point(572, 36);
+            this.comboBox1.MaxDropDownItems = 5;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(153, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(569, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Feelings and Moods Style:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 513);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(236, 13);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Report Bugs | Suggestion: ivandrofly@gmail.com";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.listViewFixes);
+            this.groupBox1.Location = new System.Drawing.Point(12, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(834, 438);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // PluginForm
+            // 
+            this.AcceptButton = this.buttonConvert;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Cancel;
+            this.ClientSize = new System.Drawing.Size(855, 535);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.checkBoxNames);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(711, 425);
+            this.Name = "PluginForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Hearing Impaired to Uppercase";
+            this.Load += new System.EventHandler(this.PluginForm_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.ListView listViewFixes;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.CheckBox checkBoxNames;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem checkAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uncheckAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem invertCheckToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteLineToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+
+    }
+}
