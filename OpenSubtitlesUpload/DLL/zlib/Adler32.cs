82a05aa6902518bab70a3181addb8ca16cfd6647
ivandrofly <ivandrofly@gmail.com>
2014-03-17 04:16:06 +0000

    zlib library cleaned

--- OpenSubtitlesUpload/DLL/zlib/Adler32.cs
+++ OpenSubtitlesUpload/DLL/zlib/Adler32.cs
@@ -4,12 +4,10 @@
 
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-// Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
-// Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
-// Neither the name of ComponentAce nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+// Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// Neither the name of ComponentAce nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
 
 /*
 Copyright (c) 2000,2001,2002,2003 ymnk, JCraft,Inc. All rights reserved.
@@ -20,8 +18,8 @@ modification, are permitted provided that the following conditions are met:
 1. Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright 
-notice, this list of conditions and the following disclaimer in 
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in
 the documentation and/or other materials provided with the distribution.
 
 3. The names of the authors may not be used to endorse or promote products
@@ -43,66 +41,64 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 * Jean-loup Gailly(jloup@gzip.org) and Mark Adler(madler@alumni.caltech.edu)
 * and contributors of zlib.
 */
-using System;
+
 namespace ComponentAce.Compression.Libs.zlib
 {
-	
-	sealed class Adler32
-	{
-		
-		// largest prime smaller than 65536
-		private const int BASE = 65521;
-		// NMAX is the largest n such that 255n(n+1)/2 + (n+1)(BASE-1) <= 2^32-1
-		private const int NMAX = 5552;
-		
-		internal long adler32(long adler, byte[] buf, int index, int len)
-		{
-			if (buf == null)
-			{
-				return 1L;
-			}
-			
-			long s1 = adler & 0xffff;
-			long s2 = (adler >> 16) & 0xffff;
-			int k;
-			
-			while (len > 0)
-			{
-				k = len < NMAX?len:NMAX;
-				len -= k;
-				while (k >= 16)
-				{
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					s1 += (buf[index++] & 0xff); s2 += s1;
-					k -= 16;
-				}
-				if (k != 0)
-				{
-					do 
-					{
-						s1 += (buf[index++] & 0xff); s2 += s1;
-					}
-					while (--k != 0);
-				}
-				s1 %= BASE;
-				s2 %= BASE;
-			}
-			return (s2 << 16) | s1;
-		}
-		
-	}
+    internal sealed class Adler32
+    {
+        // largest prime smaller than 65536
+        private const int BASE = 65521;
+
+        // NMAX is the largest n such that 255n(n+1)/2 + (n+1)(BASE-1) <= 2^32-1
+        private const int NMAX = 5552;
+
+        internal long adler32(long adler, byte[] buf, int index, int len)
+        {
+            if (buf == null)
+            {
+                return 1L;
+            }
+
+            long s1 = adler & 0xffff;
+            long s2 = (adler >> 16) & 0xffff;
+            int k;
+
+            while (len > 0)
+            {
+                k = len < NMAX ? len : NMAX;
+                len -= k;
+                while (k >= 16)
+                {
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    s1 += (buf[index++] & 0xff); s2 += s1;
+                    k -= 16;
+                }
+                if (k != 0)
+                {
+                    do
+                    {
+                        s1 += (buf[index++] & 0xff); s2 += s1;
+                    }
+                    while (--k != 0);
+                }
+                s1 %= BASE;
+                s2 %= BASE;
+            }
+            return (s2 << 16) | s1;
+        }
+    }
 }
--- OpenSubtitlesUpload/DLL/zlib/ZStreamException.cs
+++ OpenSubtitlesUpload/DLL/zlib/ZStreamException.cs
@@ -4,9 +4,9 @@
 
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-// Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
-// Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
-// Neither the name of ComponentAce nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+// Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// Neither the name of ComponentAce nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /*
@@ -18,8 +18,8 @@ modification, are permitted provided that the following conditions are met:
 1. Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright 
-notice, this list of conditions and the following disclaimer in 
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in
 the documentation and/or other materials provided with the distribution.
 
 3. The names of the authors may not be used to endorse or promote products
@@ -41,17 +41,19 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 * Jean-loup Gailly(jloup@gzip.org) and Mark Adler(madler@alumni.caltech.edu)
 * and contributors of zlib.
 */
+
 namespace ComponentAce.Compression.Libs.zlib
 {
-	
-	
-	public class ZStreamException:System.IO.IOException
-	{
-		public ZStreamException():base()
-		{
-		}
-		public ZStreamException(System.String s):base(s)
-		{
-		}
-	}
+    public class ZStreamException : System.IO.IOException
+    {
+        public ZStreamException()
+            : base()
+        {
+        }
+
+        public ZStreamException(System.String s)
+            : base(s)
+        {
+        }
+    }
 }
