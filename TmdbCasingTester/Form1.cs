b9dc674d70ad565cb3254ccc2b257d439879faac
ivandrofly <ivandrofly@gmail.com>
2014-04-11 13:13:56 +0100

    new findMoods rules

--- TmdbCasingTester/Form1.cs
+++ TmdbCasingTester/Form1.cs
@@ -17,7 +17,7 @@ namespace TmdbCasingTester
         private void button1_Click(object sender, EventArgs e)
         {
             //IPlugin plugin = new TmdbCasing();
-            IPlugin plugin = new DialogueAutoMarker();
+            IPlugin plugin = new HI2UC();
             string content = @"1
 00:01:02,939 --> 00:01:05,349
 (THUNDER RUMBLING)
