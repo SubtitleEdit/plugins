c1ecb43783cabe47e21b88ee55f0e7ee62c4466c
ivandrofly <ivandrofly@gmail.com>
2014-05-06 07:31:39 +0100

    check subtitle before loading it

--- HI2UC/Plugin.cs
+++ HI2UC/Plugin.cs
@@ -13,7 +13,7 @@ namespace Nikse.SubtitleEdit.PluginLogic
 
         string IPlugin.Description
         {
-            get { return "Convert all the text inside: \"[]\", \"()\" and \"{}\" to Uppercase"; }
+            get { return "Convert moods and Narrator to Uppercase"; }
         }
 
         string IPlugin.Name
@@ -37,31 +37,41 @@ namespace Nikse.SubtitleEdit.PluginLogic
             get { return 2.9M; }
         }
 
-        string IPlugin.DoAction(Form parentForm, string subtitle, double frameRate, string listViewLineSeparatorString, string subtitleFileName, string videoFileName, string rawText)
+        string IPlugin.DoAction(Form parentForm, string subtitle, double frameRate,
+            string listViewLineSeparatorString, string subtitleFileName, string videoFileName, string rawText)
         {
             subtitle = subtitle.Trim();
-            if (!string.IsNullOrEmpty(subtitle))
+            if (string.IsNullOrEmpty(subtitle))
             {
-                if (!string.IsNullOrEmpty(listViewLineSeparatorString))
-                    Configuration.ListViewLineSeparatorString = listViewLineSeparatorString;
-
-                var list = new List<string>();
-                foreach (string line in subtitle.Replace(Environment.NewLine, "|").Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
-                    list.Add(line);
-
-                Subtitle sub = new Subtitle();
-                SubRip srt = new SubRip();
-                srt.LoadSubtitle(sub, list, subtitleFileName);
-                using (var form = new PluginForm(parentForm, sub, (this as IPlugin).Name, (this as IPlugin).Description))
-                {
-                    if (form.ShowDialog(parentForm) == DialogResult.OK)
-                        return form.FixedSubtitle;
-                }
+                MessageBox.Show("No subtitle loaded", parentForm.Text,
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return string.Empty;
             }
 
-            MessageBox.Show("No subtitle loaded", parentForm.Text,
-                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            if (!subtitle.Contains("(") || !subtitle.Contains("["))
+            {
+                var result = MessageBox.Show("Subtitle doesn't contians Hearing Imapired notations!" + Environment.NewLine +
+                    "Do you want to continue?",
+                    "Hearing Imapired not found!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.No)
+                    return string.Empty;
+            }
+
+            if (!string.IsNullOrEmpty(listViewLineSeparatorString))
+                Configuration.ListViewLineSeparatorString = listViewLineSeparatorString;
+
+            var list = new List<string>();
+            foreach (string line in subtitle.Replace(Environment.NewLine, "|").Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+                list.Add(line);
+
+            Subtitle sub = new Subtitle();
+            SubRip srt = new SubRip();
+            srt.LoadSubtitle(sub, list, subtitleFileName);
+            using (var form = new PluginForm(parentForm, sub, (this as IPlugin).Name, (this as IPlugin).Description))
+            {
+                if (form.ShowDialog(parentForm) == DialogResult.OK)
+                    return form.FixedSubtitle;
+            }
             return string.Empty;
         }
     }
