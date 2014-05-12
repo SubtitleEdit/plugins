edaed308a270a7489b0329a0ad5857fd0bd88aae
ivandrofly <ivandrofly@gmail.com>
2014-05-12 15:48:50 +0100

    adding feat to hicolorer

--- HIColorer/Plugin.cs
+++ HIColorer/Plugin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Nikse.SubtitleEdit.PluginLogic
 {
@@ -39,7 +40,34 @@ namespace Nikse.SubtitleEdit.PluginLogic
 
         string IPlugin.DoAction(System.Windows.Forms.Form parentForm, string subtitle, double frameRate, string listViewLineSeparatorString, string subtitleFileName, string videoFileName, string rawText)
         {
-            throw new NotImplementedException();
+            subtitle = subtitle.Trim();
+            if (string.IsNullOrEmpty(subtitle))
+            {
+                MessageBox.Show("No subtitle loaded", parentForm.Text,
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return string.Empty;
+            }
+            var lines = new List<String>();
+            lines = subtitle.Replace(Environment.NewLine, "\n").Split('\n').ToList();
+            if (lines.Count < 1)
+            {
+                return string.Empty;
+            }
+            var sub = new Subtitle();
+            var subRip = new SubRip();
+            subRip.LoadSubtitle(sub, lines, subtitleFileName);
+            if (sub.Paragraphs.Count < 1)
+            {
+                return string.Empty;
+            }
+            using (var mainForm = new MainForm(sub, subtitleFileName))
+            {
+                if (mainForm.ShowDialog() == DialogResult.OK)
+                {
+                    return mainForm.FixedSubtitle;
+                }
+            }
+            return string.Empty;
         }
     }
 }
