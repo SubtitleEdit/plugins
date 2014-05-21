4c099d5d4ed193b7975dbcd6900f701ef3550a8f
ivandrofly <ivandrofly@gmail.com>
2014-05-21 21:21:01 +0100

    minor fix in utilities

--- HIColorer/Logic/Utilities.cs
+++ HIColorer/Logic/Utilities.cs
@@ -29,7 +29,7 @@ namespace Nikse.SubtitleEdit.PluginLogic
             while (s.ToLower().Contains("<font"))
             {
                 int startIndex = s.ToLower().IndexOf("<font");
-                int endIndex = Math.Max(s.IndexOf(">"), startIndex + 4);
+                int endIndex = Math.Max(s.IndexOf(">"), startIndex + 6);
                 s = s.Remove(startIndex, (endIndex - startIndex) + 1);
             }
             return s;
