ad557c698db17780b13764fe52ce0821f921b842
ivandrofly <ivandrofly@gmail.com>
2014-05-24 15:00:59 +0100

    minor update in Sub framerate

--- WordSpellCheck/DLL/Logic/Configuration.cs
+++ WordSpellCheck/DLL/Logic/Configuration.cs
@@ -2,7 +2,7 @@
 {
     internal class Configuration
     {
-        public static double CurrentFrameRate = 30;
+        public static double CurrentFrameRate = 23.976;
         public static string ListViewLineSeparatorString = "<br />";
     }
 }
