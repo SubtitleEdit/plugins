e6be6717084698d032ddd60cc73220e1118a2f05
ivandrofly <ivandrofly@gmail.com>
2014-05-26 20:17:12 +0100

    fixed: form freeze while search...

--- OpenSubtitlesUpload/DLL/ImdbSearch.cs
+++ OpenSubtitlesUpload/DLL/ImdbSearch.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using OpenSubtitles;
+using System.Threading;
 
 namespace OpenSubtitlesUpload
 {
@@ -27,7 +28,7 @@ namespace OpenSubtitlesUpload
             {
                 ImdbId = listViewSearchResults.SelectedItems[0].Text;
                 DialogResult = DialogResult.OK;
-            }            
+            }
         }
 
         private void buttonSearch_Click(object sender, EventArgs e)
@@ -35,21 +36,47 @@ namespace OpenSubtitlesUpload
             try
             {
                 Cursor = Cursors.WaitCursor;
-                var dic = _api.SearchMoviesOnIMDB(textBoxSearchQuery.Text);
-                listViewSearchResults.Items.Clear();
-                foreach (KeyValuePair<string, string> kvp in dic)
+                var backGroundWoker = new BackgroundWorker();
+                backGroundWoker.DoWork += (s, ev) =>
                 {
-                    ListViewItem item = new ListViewItem(kvp.Key);
-                    item.SubItems.Add(kvp.Value);
-                    listViewSearchResults.Items.Add(item);
-                }
-                Cursor = Cursors.Default;
+                    this.buttonSearch.BeginInvoke(new MethodInvoker(() => buttonSearch.Enabled = false));
+                    string query = ev.Argument as string;
+                    Dictionary<string, string> dic = null;
+                    //ThreadPool.QueueUserWorkItem(_ =>
+                    //{
+                    //});
+                    dic = _api.SearchMoviesOnIMDB(textBoxSearchQuery.Text);
+                    ev.Result = dic;
+                };
+
+                backGroundWoker.RunWorkerCompleted += (s, ev) =>
+                {
+                    var dic = ev.Result as Dictionary<string, string>;
+                    if (dic.Count == 0)
+                    {
+                        MessageBox.Show("Movie/Tv-Show not found!!!", "Not fond", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        Cursor = Cursors.Default;
+                        this.buttonSearch.BeginInvoke(new MethodInvoker(() => buttonSearch.Enabled = true));
+                        return;
+                    }
+
+                    listViewSearchResults.Items.Clear();
+                    foreach (KeyValuePair<string, string> kvp in dic)
+                    {
+                        ListViewItem item = new ListViewItem(kvp.Key);
+                        item.SubItems.Add(kvp.Value);
+                        listViewSearchResults.Items.Add(item);
+                    }
+                    this.buttonSearch.BeginInvoke(new MethodInvoker(() => buttonSearch.Enabled = true));
+                };
+
+                backGroundWoker.RunWorkerAsync(textBoxSearchQuery.Text);
             }
             catch (Exception exception)
             {
-                Cursor = Cursors.Default;
                 MessageBox.Show(exception.Message);
             }
+            Cursor = Cursors.Default;
         }
 
         private void listViewSearchResults_MouseDoubleClick(object sender, MouseEventArgs e)
