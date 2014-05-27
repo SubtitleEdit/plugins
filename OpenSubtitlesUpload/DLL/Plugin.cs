6c5e58366e8fe2558eedd9b2c0807a15ea897773
ivandrofly <ivandrofly@gmail.com>
2014-05-27 15:12:04 +0100

    ver file updated

--- OpenSubtitlesUpload/DLL/Plugin.cs
+++ OpenSubtitlesUpload/DLL/Plugin.cs
@@ -18,7 +18,7 @@ namespace Nikse.SubtitleEdit.PluginLogic
 
         decimal IPlugin.Version
         {
-            get { return 1.7M; }
+            get { return 1.8M; }
         }
 
         string IPlugin.Description
