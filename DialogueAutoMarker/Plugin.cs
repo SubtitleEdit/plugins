783ed3a5032c101f1292c5b34f8cab99af0285b1
ivandrofly <ivandrofly@gmail.com>
2014-05-30 18:10:05 +0100

    some refac in Intry point

--- DialogueAutoMarker/Plugin.cs
+++ DialogueAutoMarker/Plugin.cs
@@ -36,34 +36,32 @@ namespace Nikse.SubtitleEdit.PluginLogic
         //Gets or sets the major, minor, build, and revision numbers of the assembly.
         decimal IPlugin.Version
         {
-            get { return 0.1M; }
+            get { return 0.2M; }
         }
 
         string IPlugin.DoAction(Form parentForm, string subtitle, double frameRate, string listViewLineSeparatorString, string subtitleFileName, string videoFileName, string rawText)
         {
-            subtitle = subtitle.Trim();
-            if (!string.IsNullOrEmpty(subtitle))
+            if (string.IsNullOrEmpty(subtitle))
             {
-                if (!string.IsNullOrEmpty(listViewLineSeparatorString))
-                    Configuration.ListViewLineSeparatorString = listViewLineSeparatorString;
-
-                var list = new List<string>();
-                foreach (string line in subtitle.Replace(Environment.NewLine, "\n").Split('\n'))
-                    list.Add(line);
-
-                Subtitle sub = new Subtitle();
-                SubRip srt = new SubRip();
-                srt.LoadSubtitle(sub, list, subtitleFileName);
-                using (var form = new Main(parentForm, sub, (this as IPlugin).Name, (this as IPlugin).Description))
-                {
-                    if (form.ShowDialog(parentForm) == DialogResult.OK)
-                        return form.FixedSubtitle;
-                }
+                MessageBox.Show("No subtitle loaded", parentForm.Text,
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return string.Empty;
             }
+            if (!string.IsNullOrEmpty(listViewLineSeparatorString))
+                Configuration.ListViewLineSeparatorString = listViewLineSeparatorString;
 
-            MessageBox.Show("No subtitle loaded", parentForm.Text,
-                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            var list = new List<string>();
+            foreach (string line in subtitle.Replace(Environment.NewLine, "\n").Split('\n'))
+                list.Add(line);
+
+            Subtitle sub = new Subtitle();
+            SubRip srt = new SubRip();
+            srt.LoadSubtitle(sub, list, subtitleFileName);
+            using (var form = new PluginForm(parentForm, sub, (this as IPlugin).Name, (this as IPlugin).Description))
+            {
+                if (form.ShowDialog(parentForm) == DialogResult.OK)
+                    return form.FixedSubtitle;
+            }
             return string.Empty;
         }
     }
