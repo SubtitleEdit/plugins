3d8a469779cdc94faedb599155f10a11f41cae5e
ivandrofly <ivandrofly@gmail.com>
2015-03-05 21:07:50 +0000

    Plugin: AmericaToBritish Updated v0.2

--- AmericanToBritish/DLL/Properties/AssemblyInfo.cs
+++ AmericanToBritish/DLL/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LinesUnbreaker")]
+[assembly: AssemblyTitle("American To British")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
