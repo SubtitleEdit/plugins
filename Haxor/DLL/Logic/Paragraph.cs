948df5c32f04534c3c4cd0c70399529bc45c70f7
ivandrofly <ivandrofly@gmail.com>
2015-05-19 19:02:01 +0100

    Internal field not 'needed' once class is set to internal + Unneeded code removed

--- Haxor/DLL/Logic/Paragraph.cs
+++ Haxor/DLL/Logic/Paragraph.cs
@@ -4,11 +4,11 @@ namespace Nikse.SubtitleEdit.PluginLogic
 {
     internal class Paragraph
     {
-        internal int Number { get; set; }
-        internal string Text { get; set; }
-        internal TimeCode StartTime { get; set; }
-        internal TimeCode EndTime { get; set; }
-        internal TimeCode Duration
+        public int Number { get; set; }
+        public string Text { get; set; }
+        public TimeCode StartTime { get; set; }
+        public TimeCode EndTime { get; set; }
+        public TimeCode Duration
         {
             get
             {
@@ -17,28 +17,21 @@ namespace Nikse.SubtitleEdit.PluginLogic
                 return timeCode;
             }
         }
-        internal int StartFrame { get; set; }
-        internal int EndFrame { get; set; }
-        internal bool Forced { get; set; }
-        internal string Extra { get; set; }
-        internal bool IsComment { get; set; }
-        internal string Actor { get; set; }
+        public int StartFrame { get; set; }
+        public int EndFrame { get; set; }
+        public bool Forced { get; set; }
+        public string Extra { get; set; }
+        public bool IsComment { get; set; }
+        public string Actor { get; set; }
 
-        internal Paragraph()
+        public Paragraph()
         {
             StartTime = new TimeCode(TimeSpan.FromSeconds(0));
             EndTime = new TimeCode(TimeSpan.FromSeconds(0));
             Text = string.Empty;
         }
 
-        internal Paragraph(TimeCode startTime, TimeCode endTime, string text)
-        {
-            StartTime = startTime;
-            EndTime = endTime;
-            Text = text;
-        }
-
-        internal Paragraph(Paragraph paragraph)
+        public Paragraph(Paragraph paragraph)
         {
             Number = paragraph.Number;
             Text = paragraph.Text;
@@ -52,16 +45,7 @@ namespace Nikse.SubtitleEdit.PluginLogic
             Actor = paragraph.Actor;
         }
 
-        internal Paragraph(int startFrame, int endFrame, string text)
-        {
-            StartTime = new TimeCode(0, 0, 0, 0);
-            EndTime = new TimeCode(0, 0, 0, 0);
-            StartFrame = startFrame;
-            EndFrame = endFrame;
-            Text = text;
-        }
-
-        internal Paragraph(string text, double startTotalMilliseconds, double endTotalMilliseconds)
+        public Paragraph(string text, double startTotalMilliseconds, double endTotalMilliseconds)
         {
             StartTime = new TimeCode(TimeSpan.FromMilliseconds(startTotalMilliseconds));
             EndTime = new TimeCode(TimeSpan.FromMilliseconds(endTotalMilliseconds));
@@ -75,7 +59,7 @@ namespace Nikse.SubtitleEdit.PluginLogic
             //return StartTime + " --> " + EndTime + " " + Text;
         }
 
-        internal int NumberOfLines
+        public int NumberOfLines
         {
             get
             {
