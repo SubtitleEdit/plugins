677bb5151165a44480e9ae2dfc2a468e11f9712a
Nikolaj Olsson <nikse.dk@gmail.com>
2016-02-13 22:24:49 +0100

    Working on cleaning up plugins

---
+++ NamesListManager/DLL/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Nikse.SubtitleEdit.PluginLogic
+{
+    public partial class Form1 : Form
+    {
+        private string _namesFileName;
+        private List<string> _namesList;
+        private XDocument _namesXml;
+        private bool _changed;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            labelCount.Text = string.Empty;
+            labelImportCount.Text = string.Empty;
+            labelTransfered.Text = string.Empty;
+            string defaultFileName = @"C:\Data\SubtitleEdit\subtitleedit\Dictionaries\names_etc.xml";
+            if (File.Exists(defaultFileName))
+            {
+                LoadNamesFile(defaultFileName);
+            }
+        }
+
+        private void LoadNamesFile(string fileName)
+        {
+            textBoxFileName.Text = fileName;
+            _namesFileName = fileName;
+            _namesXml = XDocument.Load(fileName);
+            _namesList = new List<string>();
+            if (_namesXml.Document != null)
+            {
+                foreach (var nameNode in _namesXml.Document.Descendants("name"))
+                {
+                    string s = nameNode.Value;
+                    if (!_namesList.Contains(s))
+                        _namesList.Add(s);
+                }
+            }
+            FillNames();
+            labelCount.Text = _namesList.Count.ToString();
+            _changed = false;
+        }
+
+        private void FillNames()
+        {
+            _namesList.Sort();
+            listBoxNames.Items.Clear();
+            listBoxNames.BeginUpdate();
+            foreach (var name in _namesList)
+            {
+                listBoxNames.Items.Add(name);
+            }
+            listBoxNames.EndUpdate();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string name = textBoxNewWord.Text.Trim();
+            if (name.Length < 1 || _namesList == null || _namesXml?.Document == null || _namesFileName == null)
+            {
+                return;
+            }
+
+            if (_namesList.Contains(name))
+            {
+                MessageBox.Show($"The name {name} already exists in the names list");
+                return;
+            }
+
+            _namesList.Add(name);
+            FillNames();
+            textBoxNewWord.Text = string.Empty;
+            textBoxNewWord.Focus();
+            listBoxNames.SelectedIndex = _namesList.IndexOf(name);
+            _changed = true;
+        }
+
+        private void SaveNames()
+        {
+            try
+            {
+                if (File.Exists(_namesFileName))
+                {
+                    File.Copy(_namesFileName, _namesFileName + ".bak", true);
+                }
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+
+            _namesXml.Document?.Descendants("name").Remove();
+            var root = _namesXml.Descendants().First(p => p.NodeType == XmlNodeType.Element);
+            foreach (var name in _namesList)
+            {
+                root.Add(new XElement("name", name));
+            }
+            _namesXml.Save(_namesFileName);
+            MessageBox.Show(_namesFileName + " saved!");
+        }
+
+        private void listBoxNames_KeyDown(object sender, KeyEventArgs e)
+        {
+            int index = listBoxNames.SelectedIndex;
+            if (index < 0)
+            {
+                return;
+            }
+
+            string name = listBoxNames.Items[index].ToString();
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (MessageBox.Show("Delete '" + name + "' ?", string.Empty, MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    _namesList.Remove(name);
+                    FillNames();
+                    _changed = true;
+                }
+            }
+        }
+
+        private void listBoxNames_DoubleClick(object sender, EventArgs e)
+        {
+            var lb = (sender as ListBox);
+            if (lb == null)
+            {
+                return;
+            }
+            int index = lb.SelectedIndex;
+            if (index < 0)
+            {
+                return;
+            }
+
+            string name = lb.Items[index].ToString();
+            Process.Start("https://www.google.com/search?q=" + Uri.EscapeUriString(name));
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveNames();
+            _changed = false;
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Names etc files|*names_etc*.xml";
+            openFileDialog1.FileName = string.Empty;
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                LoadNamesFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Names files|*names_etc*.xml|Text files|*.txt";
+            openFileDialog1.FileName = string.Empty;
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                listBoxImport.Items.Clear();
+                int countSkippedExisting = 0;
+                int countSkippedOneLetter = 0;
+                List<string> importedList = new List<string>();
+                if (openFileDialog1.FileName.EndsWith(".xml", StringComparison.OrdinalIgnoreCase))
+                {
+                    var xdoc = XDocument.Load(openFileDialog1.FileName);
+                    if (xdoc.Document != null)
+                    {
+                        foreach (var nameNode in xdoc.Document.Descendants("name"))
+                        {
+                            string s = nameNode.Value;
+                            while (s.Contains("  "))
+                                s = s.Replace("  ", " ");
+                            if (_namesList.Contains(s))
+                            {
+                                countSkippedExisting++;
+                            }
+                            else if (s.Length < 2)
+                            {
+                                countSkippedOneLetter++;
+                            }
+                            else
+                            {
+                                if (!importedList.Contains(s))
+                                {
+                                    importedList.Add(s);
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var line in File.ReadAllLines(openFileDialog1.FileName))
+                    {
+                        string s = line.Trim();
+                        while (s.Contains("  "))
+                            s = s.Replace("  ", " ");
+                        if (_namesList.Contains(s))
+                        {
+                            countSkippedExisting++;
+                        }
+                        else if (s.Length < 2)
+                        {
+                            countSkippedOneLetter++;
+                        }
+                        else
+                        {
+                            if (!importedList.Contains(s))
+                            {
+                                importedList.Add(s);
+                            }
+                        }
+                    }
+                }
+                importedList.Sort();
+                listBoxImport.BeginUpdate();
+                foreach (var s in importedList)
+                {
+                    listBoxImport.Items.Add(s);
+                }
+                listBoxImport.EndUpdate();
+
+                labelImportCount.Text = listBoxImport.Items.Count.ToString();
+                MessageBox.Show("Words found: " + listBoxImport.Items.Count + Environment.NewLine + 
+                                "Existing words skipped: " + countSkippedExisting + Environment.NewLine + 
+                                "One letter word skipped: " + countSkippedOneLetter);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_namesList == null || _namesXml?.Document == null || _namesFileName == null)
+            {
+                return;
+            }
+
+            int numberOfWordsAdded = 0;
+            foreach (int index in listBoxImport.SelectedIndices)
+            {
+                string name = listBoxImport.Items[index].ToString();
+                if (!_namesList.Contains(name))
+                {
+                    _namesList.Add(name);
+                    numberOfWordsAdded++;
+                    _changed = true;
+                }
+            }
+            FillNames();
+            labelTransfered.Text = "Words added: " + numberOfWordsAdded;
+            timer1.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = DialogResult.No;
+            if (_changed)
+            {
+                result = MessageBox.Show("Do you want to save changes?", string.Empty, MessageBoxButtons.YesNoCancel);
+            }
+            if (result == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+                return;
+            }
+            if (result == DialogResult.Yes)
+            {
+                SaveNames();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelTransfered.Text = string.Empty;
+            timer1.Stop();
+        }
+
+    }
+}
---
+++ NamesListManager/DLL/Plugin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Nikse.SubtitleEdit.PluginLogic
+{
+    public class NamesListManager : IPlugin // dll file name must "<classname>.dll" - e.g. "Haxor.dll"
+    {
+        string IPlugin.Name
+        {
+            get { return "NamesListManager"; }
+        }
+
+        string IPlugin.Text
+        {
+            get { return "Names list manager"; }
+        }
+
+        decimal IPlugin.Version
+        {
+            get { return 1.5M; }
+        }
+
+        string IPlugin.Description
+        {
+            get { return "Add/delete names from names_etc list"; }
+        }
+
+        string IPlugin.ActionType // Can be one of these: file, tool, sync, translate, spellcheck
+        {
+            get { return "spellcheck"; }
+        }
+
+        string IPlugin.Shortcut
+        {
+            get { return string.Empty; }
+        }
+
+        string IPlugin.DoAction(Form parentForm, string subtitle, double frameRate, string listViewLineSeparatorString, string subtitleFileName, string videoFileName, string rawText)
+        {
+            subtitle = subtitle.Trim();
+
+            if (!string.IsNullOrEmpty(listViewLineSeparatorString))
+                Configuration.ListViewLineSeparatorString = listViewLineSeparatorString;
+
+            var list = new List<string>();
+            foreach (string line in subtitle.Replace(Environment.NewLine, "\n").Split('\n'))
+                list.Add(line);
+
+            var sub = new Subtitle();
+            var srt = new SubRip();
+            srt.LoadSubtitle(sub, list, subtitleFileName);
+            using (var form = new Form1())
+            {
+                form.ShowDialog(parentForm);
+            }
+            return string.Empty;
+        }
+    }
+}
