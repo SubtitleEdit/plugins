8cf125d73b440afe4cda392ce31dff2f79cad63f
Nikolaj Olsson <nio@bluegarden.com>
2018-12-20 08:46:31 +0100

    Add new plugin

---
+++ BTScreenScraper/DLL/Translator/TranslationPair.cs
@@ -0,0 +1,26 @@
+﻿namespace WebViewTranslate.Translator
+{
+    public class TranslationPair
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public TranslationPair()
+        {
+            
+        }
+
+        public TranslationPair(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public override string ToString()
+        {
+            if (Name != null && Name.Length > 1)
+                return Name.Substring(0, 1).ToUpperInvariant() + Name.Substring(1).ToLowerInvariant();
+            return Name;
+        }
+    }
+}
