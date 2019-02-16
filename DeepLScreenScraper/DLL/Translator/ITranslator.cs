04c070976e6f6df6b7c6309f99334d644da0b72c
Nikolaj Olsson <nikse.dk@gmail.com>
2019-02-16 19:26:48 +0100

    Add DeepL screen-scraper

---
+++ DeepLScreenScraper/DLL/Translator/ITranslator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using WebViewTranslate.Logic;
+
+namespace WebViewTranslate.Translator
+{
+    public interface ITranslator
+    {
+        List<TranslationPair> GetTranslationPairs();
+        string GetName();
+        string GetUrl();
+        List<string> Translate(string sourceLanguage, string targetLanguage, List<Paragraph> paragraphs, StringBuilder log);
+    }
+}
