3ab86df8387a06f96464af7d9575e4c7ba084480
Ivandro Ismael <ivandrofly@gmail.com>
2019-03-20 17:41:05 +0000

    add example code on how to expose SE method

---
+++ ExposeSEApi/EntryPoint.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Nikse.SubtitleEdit.PluginLogic
+{
+    public class ExposeSEApi : IPlugin
+    {
+        private Func<string, bool, string> RemoveHtmlTags;
+
+        public string Name => nameof(ExposeSEApi);
+        public string Text => nameof(ExposeSEApi);
+        public decimal Version => .1m;
+        public string Description => "Exposed Subtitle Edit APIs using expression/reflection";
+        public string ActionType => "tool";
+        public string Shortcut => string.Empty;
+
+        public string DoAction(Form parentForm, string srtText, double frameRate, string uiLineBreak, string file, string videoFile, string rawText)
+        {
+            // the type of Main.cs
+            Type parentType = parentForm.GetType();
+
+            // get the assembly where type of Main is defined
+            Assembly assembly = parentType.Assembly;
+
+            // get the HtmlUtil type
+            Type htmlUtilType = assembly.GetType("Nikse.SubtitleEdit.Core.HtmlUtil");
+
+            // build parameters
+            ParameterExpression inputExp = Expression.Parameter(typeof(string), "input");
+            ParameterExpression removeASSTag = Expression.Parameter(typeof(bool), "removeAssTag");
+
+            // build method call
+            MethodInfo removeHtmltagsMI = htmlUtilType.GetMethod("RemoveHtmlTags", BindingFlags.Public | BindingFlags.Static);
+            MethodCallExpression methodCallExp = Expression.Call(removeHtmltagsMI, inputExp, removeASSTag);
+
+            // build/compile lambda to function
+            RemoveHtmlTags = Expression.Lambda<Func<string, bool, string>>(methodCallExp, inputExp, removeASSTag).Compile();
+
+            string text = "<i>Fooobar</i>";
+
+            // call the method Subtitle Edit RemoveHtmlTags method from HtmlUTil to remove html tags
+            text = RemoveHtmlTags(text, true);
+
+            MessageBox.Show(text);
+
+            return string.Empty;
+        }
+    }
+}
---
+++ ExposeSEApi/IPlugin.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Forms;
+
+namespace Nikse.SubtitleEdit.PluginLogic
+{
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Name of the plug-in
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Text used in Subtitle Edit menu
+        /// </summary>
+        string Text { get; }
+
+        /// <summary>
+        /// Version number of plugin
+        /// </summary>
+        decimal Version { get; }
+
+        /// <summary>
+        /// Description of what plugin does
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// Can be one of these: file, tool, sync, translate, spellcheck
+        /// </summary>
+        string ActionType { get; }
+
+        /// <summary>
+        /// Shortcut used to active plugin - e.g. Control+Shift+F9
+        /// </summary>
+        string Shortcut { get; }
+
+        /// <summary>
+        /// This action of callsed when Subtitle Edit calls plugin
+        /// </summary>
+        /// <param name="parentForm">Main form in Subtitle Edit</param>
+        /// <param name="srtText">SubRip text</param>
+        /// <param name="frameRate">Current frame rate</param>
+        /// <param name="uiLineBreak"></param>
+        /// <param name="file">Current subtitle file name</param>
+        /// <param name="videoFile">Current video file name</param>
+        /// <param name="rawText">Subtitle raw format</param>
+        /// <returns>Dialog</returns>
+        string DoAction(Form parentForm,
+            string srtText,
+            double frameRate,
+            string uiLineBreak,
+            string file,
+            string videoFile,
+            string rawText);
+    }
+}
