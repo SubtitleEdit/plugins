83cd3aafb3b27f2789cefd5d4c477ed1bc4634c3
Ivandro IG Jao <ivandrofly@gmail.com>
2020-08-31 23:06:12 +0100

    dropbox: update both load and save

--- DropBoxSave/Plugin.cs
+++ DropBoxSave/Plugin.cs
@@ -18,7 +18,7 @@ namespace Nikse.SubtitleEdit.PluginLogic
 
         decimal IPlugin.Version
         {
-            get { return 1.3M; }
+            get { return 1.4M; }
         }
 
         string IPlugin.Description
