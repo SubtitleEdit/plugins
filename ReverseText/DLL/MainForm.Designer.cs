9a83a96d1dcf6500027d6f0a656ff792f773eaf4
niksedk <nikse.dk@gmail.com>
2022-05-15 18:54:40 +0200

    Add reverse text

---
+++ ReverseText/DLL/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ReverseText
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSource = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderTarget = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderNumber,
+            this.columnHeaderSource,
+            this.columnHeaderTarget});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(15, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(876, 479);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Resize += new System.EventHandler(this.listView1_Resize);
+            // 
+            // columnHeaderNumber
+            // 
+            this.columnHeaderNumber.Text = "Line #";
+            this.columnHeaderNumber.Width = 48;
+            // 
+            // columnHeaderSource
+            // 
+            this.columnHeaderSource.Text = "Original";
+            this.columnHeaderSource.Width = 350;
+            // 
+            // columnHeaderTarget
+            // 
+            this.columnHeaderTarget.Text = "Reversed";
+            this.columnHeaderTarget.Width = 335;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.Location = new System.Drawing.Point(735, 525);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 7;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(816, 525);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 8;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.buttonOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(903, 556);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.listView1);
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(870, 500);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResizeEnd += new System.EventHandler(this.MainForm_ResizeEnd);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderNumber;
+        private System.Windows.Forms.ColumnHeader columnHeaderSource;
+        private System.Windows.Forms.ColumnHeader columnHeaderTarget;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ ReverseText/DLL/MainForm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
+using WebViewTranslate.Logic;
+
+namespace ReverseText
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        private readonly Subtitle _subtitle;
+
+        public string FixedSubtitle { get; private set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        public MainForm(Subtitle sub, string title, string description, Form parentForm)
+            : this()
+        {
+            Text = title;
+            _subtitle = sub;
+            GeneratePreview();
+        }
+
+        public static string Reverse(string s)
+        {
+            var sb = new StringBuilder();
+            foreach (var line in s.SplitToLines())
+            {
+                var charArray = s.ToCharArray();
+                Array.Reverse(charArray);
+                sb.AppendLine(new string(charArray));
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public sealed override string Text
+        {
+            get => base.Text;
+            set => base.Text = value;
+        }
+
+        private void GeneratePreview()
+        {
+            for (var index = 0; index < _subtitle.Paragraphs.Count; index++)
+            {
+                var p = _subtitle.Paragraphs[index];
+                var before = p.Text;
+                var after = Reverse(p.Text);
+                AddToListView(p, before, after);
+            }
+        }
+
+        private void AddToListView(Paragraph p, string before, string after)
+        {
+            var item = new ListViewItem(p.Number.ToString(CultureInfo.InvariantCulture))
+            {
+                Tag = p,
+                Checked = true
+            };
+            item.SubItems.Add(p.Text.Replace(Environment.NewLine, "<br />"));
+            item.SubItems.Add(after.Replace(Environment.NewLine, "<br />"));
+            listView1.Items.Add(item);
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            for (var index = 0; index < _subtitle.Paragraphs.Count; index++)
+            {
+                var item = listView1.Items[index];
+                if (item.Checked)
+                {
+                    var p = _subtitle.Paragraphs[index];
+                    p.Text = Reverse(p.Text);
+                }
+            }
+
+            FixedSubtitle = _subtitle.ToText(new SubRip());
+            DialogResult = DialogResult.OK;
+        }
+
+        private void listView1_Resize(object sender, EventArgs e)
+        {
+            var size = (listView1.Width - listView1.Columns[0].Width) >> 2;
+            listView1.Columns[1].Width = size;
+            listView1.Columns[2].Width = -2;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                DialogResult = DialogResult.Cancel;
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            var subtract = listView1.Columns[0].Width + 20;
+            var width = listView1.Width / 2 - subtract;
+            listView1.Columns[1].Width = width;
+            listView1.Columns[1].Width = width;
+            listView1.Columns[2].Width = width;
+            listView1.Columns[1].Width = width;
+            listView1.Columns[1].Width = width;
+        }
+
+        private void MainForm_ResizeEnd(object sender, EventArgs e)
+        {
+            MainForm_Resize(sender, e);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            MainForm_Resize(sender, e);
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            MainForm_Resize(sender, e);
+        }
+    }
+}
