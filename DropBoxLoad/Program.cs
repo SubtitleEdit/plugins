ed3015c0fc3e9af8344678559ac9ff9015d755be
niksedk <nikse.dk@gmail.com>
2022-12-25 10:52:35 +0100

    Remove dropbox plugin

---
+++ DropBoxLoad/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using Nikse.SubtitleEdit.PluginLogic;
+
+namespace Dropbox.Api
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var fileName = string.Empty;
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                fileName = args[1];
+            }
+
+            var form = new PluginForm("DropBox Load", "Load subtitle from DropBox");
+            form.ShowDialog();
+        }
+    }
+}
