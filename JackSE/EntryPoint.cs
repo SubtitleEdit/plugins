20151c0d89b7da76c408291bee575955a880819c
Ivandro Jao <ivandrofly@gmail.com>
2023-10-09 01:46:22 +0100

    Remove unused usings across multiple files
    Removed unused 'System.Runtime.CompilerServices', 'Microsoft.Win32', 'System.Threading' and 'System.Threading.Tasks' usings in AssemblyInfo.cs and EntryPoint.cs to clean up the codebase and improve readability.

--- JackSE/EntryPoint.cs
+++ JackSE/EntryPoint.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
