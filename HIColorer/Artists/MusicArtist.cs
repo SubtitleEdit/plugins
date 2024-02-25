8c75f0d715d14c0908b634dc297b65c860b238e5
Ivandro Jao <ivandrofly@gmail.com>
2024-02-25 15:17:34 +0000

    Refactor and improve color handling in MusicArtist.cs
    Updated the condition within the while loop for better readability. Introduced a new method, IsAlreadyColoured, to check if the text is already colored and ensure no changes are made to it. This is important when there are both mood and music symbols in the same text.

--- HIColorer/Artists/MusicArtist.cs
+++ HIColorer/Artists/MusicArtist.cs
@@ -22,18 +22,31 @@ namespace Nikse.SubtitleEdit.PluginLogic
                 var i = 0;
                 var len = text.Length;
 
-                // todo: move these into 'SkipText' struct?
-                while (i < len && text[i] == '<' || text[i] == '{')
+                while (i < len && (text[i] == '<' || text[i] == '{'))
                 {
                     i = Math.Max(text.IndexOf(GetClosingPair(text[i]), i + 1) + 1, 0);
                 }
-
-                var skipText = ToSkipText(text, i);
-                if (IsMusicText(skipText))
+                
+                // note if full text is already coloured do not change the color
+                // this might happen when there is both mood and music symbols in same text
+                if (!IsAlreadyColoured(text) && IsMusicText(ToSkipText(text, i)))
                 {
                     p.Text = ApplyColor(_palette.Color, p.Text);
                 }
             }
+        }
+
+        private static bool IsAlreadyColoured(string text)
+        {
+            var len = text.Length;
+            
+            //<font color="#ffff00">
+            if (len < 15) return false;
+
+            var closeIdx = text.IndexOf('>', 1);
+            if (closeIdx < 0) return false;
+            var tag = text.Substring(0, closeIdx + 1);
+            return tag.Contains("color=", StringComparison.OrdinalIgnoreCase);
         }
 
         private static char GetClosingPair(char ch) => ch == '<' ? '>' : '}';
