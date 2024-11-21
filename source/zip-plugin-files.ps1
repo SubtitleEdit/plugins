c19ebc5a604d6e75f1c3661aa8b897b1e86889d3
Ivandro Jao <Ivandrofly@gmail.com>
2024-11-21 11:49:31 +0000

    Add new directories to zip script
    Included "TightHyphen" and "NoRecursiveDash" directories to the list of paths to be zipped. This change ensures that the output of these projects is included in the final zip file.

--- source/zip-plugin-files.ps1
+++ source/zip-plugin-files.ps1
@@ -2,6 +2,8 @@
 $directories = @(
     "source\HI2UC\bin\Release\net48",
     "source\HIColorer\bin\Release\net48",
+    "source\TightHyphen\bin\Release\net48",
+    "source\NoRecursiveDash\bin\Release\net48",
     "source\LinesUnbreaker\bin\Release\net48"
 )
 
