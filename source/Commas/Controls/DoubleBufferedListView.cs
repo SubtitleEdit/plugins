7e0a4683a46c5188a5ae8244f6ee5ac0d068efcf
Ivandro Jao <Ivandrofly@gmail.com>
2025-02-14 04:15:11 +0000

    Enable double buffering for custom ListView control.
    Added XML documentation and ensured double buffering is enabled to reduce visual flickering during frequent updates. Improves rendering performance for scenarios with constant updates.

--- source/Commas/Controls/DoubleBufferedListView.cs
+++ source/Commas/Controls/DoubleBufferedListView.cs
@@ -1,9 +1,16 @@
 ﻿namespace Nikse.SubtitleEdit.PluginLogic.Controls;
 
+/// <summary>
+/// A custom implementation of the ListView control with double buffering enabled.
+/// </summary>
+/// <remarks>
+/// This class inherits from ListView and overrides its double-buffering behavior to reduce flickering during frequent updates or additions of items to the control.
+/// </remarks>
 public class DoubleBufferedListView : ListView
 {
     public DoubleBufferedListView()
     {
+        // to fix flickering when nearly constant refresh listview when adding items 
         DoubleBuffered = true;
     }
 }
