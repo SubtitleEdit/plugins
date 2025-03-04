c986f80d5451afd8322161cdb6190b5f50d74bc6
Ivandro Jao <Ivandrofly@gmail.com>
2025-03-04 18:38:34 +0000

    Remove unused using directives in EntryPoint.cs
        Unused using directives for System, System.IO, and System.Reflection were removed to clean up the code. This improves readability and ensures adherence to good coding practices. No functional changes were made.

--- source/HI2UC/EntryPoint.cs
+++ source/HI2UC/EntryPoint.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Reflection;
 using System.Windows.Forms;
 using Nikse.SubtitleEdit.PluginLogic.Services;
 
